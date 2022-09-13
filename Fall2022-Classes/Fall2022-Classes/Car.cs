badef7edfbc479d77cb7302fd4ffd4a52210d7cd
Kolton Koester <koltonkoester10@gmail.com>
2022-09-13 11:57:52 -0400

    added constructors to car class

--- Fall2022-Classes/Fall2022-Classes/Car.cs
+++ Fall2022-Classes/Fall2022-Classes/Car.cs
@@ -15,7 +15,36 @@ namespace Fall2022_Classes
         public string Color { get; set; }
         public int Speed { get; set; }
         public bool IsPaidFor { get; set; }
-        
+        //'prop' short hand to creat a property 
+        // public int MyProperty { get; set; }
+
+        // access modifiers: Public, Private, interal
+        // how we can access classes, properties, methods, ect. in our application 
+
+        // constructors 
+        public Car()
+        {
+            Make = "Lexus";
+            Model = "rx 350";
+            Color = "white";
+            Year = 2018;
+            Speed = 0;
+            IsPaidFor = false;
+        }
+        // method overloading
+        public Car(string make, string model, string color, int year, int speed, bool isPaidFor)
+        {
+            Make = make;
+            Model = model;
+            Color = color;
+            Year = year;
+            Speed = speed;
+            IsPaidFor = isPaidFor;
+        }
+
         // Methods
+
+
+
     }
 }
--- Fall2022-Classes/Fall2022-Classes/Program.cs
+++ Fall2022-Classes/Fall2022-Classes/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using Fall2022_Classes;
+// creating a car with our default constructor
+Car ashleysCar = new Car();
+
+Console.WriteLine(ashleysCar.Make);
+
+ashleysCar.IsPaidFor = true;
+Console.WriteLine("This car is paid for? : " + ashleysCar.IsPaidFor);
+
+var gavCarMake = "Toyota";
+//creating another car with a different constructor
+Car gavinsCar = new Car(gavCarMake, "Camry", "Grey", 2007, 0, false);
+
+Console.WriteLine("Gavin has a " + gavinsCar.Make);
+
+
+// making a list of objects from out new class
+var garage = new List<Car>();
+// adding our cars to the List
+garage.Add(ashleysCar);
+garage.Add(gavinsCar);
+//showing we can access those cars and their properties from our List
+Console.WriteLine(garage[0].Model);
+

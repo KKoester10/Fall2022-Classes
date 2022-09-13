7be36de1e628b265b4a03e284e1278c8be0c7d60
Kolton Koester <koltonkoester10@gmail.com>
2022-09-13 11:12:23 -0400

    Created a car obj with properties

---
+++ Fall2022-Classes/Fall2022-Classes/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall2022_Classes
+{
+    public class Car
+    {
+        // properties
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; } 
+        public string Color { get; set; }
+        public int Speed { get; set; }
+        public bool IsPaidFor { get; set; }
+        
+        // Methods
+    }
+}
